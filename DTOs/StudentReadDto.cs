ea589bd58c84d9884ddf24e670101c60c715d904
ZiyadAlboghdady <ziyadalboghdady@gmail.com>
2026-03-09 13:42:58 +0300

    Add StudentReadDto and map Student entity to DTO in Students API

---
+++ DTOs/StudentReadDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demoEFapp.DTOs
+{
+    public class StudentReadDto : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
